5c92327d3b5acb7c04f62b965ce8e8490900298f
kellerx <keller0319@gmail.com>
2018-03-29 10:05:36 +0800

    add test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
-import tdata.data as data
+from tdata import local
+from tdata.remote_service import ds
 
 
 def test_daily_table():
-    assert len(data.local_daily('000001.SH')) == len(
-        data.ds.query_trade_dates(19901219, data.local_last_date()))
+    assert len(local.daily('000001.SH')) == len(
+        ds.query_trade_dates(19901219, local.daily_last_date()))
