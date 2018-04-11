f1f63d9f3eccf19a1f2f7bdeca4d4d3651f8e24a
kellerx <keller0319@gmail.com>
2018-04-11 22:36:17 +0800

    ogm models of company and concept

--- tdata/graph_proxy.py
+++ tdata/graph_proxy.py
@@ -1,6 +1,9 @@
 # query data from neo4j graph
 
+# TODO: model and migrate
+
 import py2neo as neo
+from py2neo import ogm
 
 graph = neo.Graph(password='ivey198013')
 selector = neo.NodeSelector(graph)
@@ -11,3 +14,23 @@ def query_all_concepts() -> list:
 
 def query_new_concepts(delta):
     pass
+
+def query_related_nodes(depth=1):
+    pass
+
+class Company(ogm.GraphObject):
+    __primarykey__ = 'name'
+
+    name = ogm.Property()
+    sid = ogm.Property()
+    created = ogm.Property()
+
+    concepts = ogm.RelatedTo('Concept', 'CONCEPT')
+
+class Concept(ogm.GraphObject):
+    __primarykey__ = 'name'
+
+    name = ogm.Property()
+    created = ogm.Property()
+
+    companys = ogm.RelatedFrom('Company', 'CONCEPT')
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -2,14 +2,21 @@ from tdata import graph_proxy as tgraph
 import py2neo as neo
 import pytest
 
+
 @pytest.fixture(scope='module')
 def selector():
     graph = neo.Graph(password='ivey198013')
     return neo.NodeSelector(graph)
 
+
 def test_query_all_concepts(selector):
     selection = selector.select('Concept')
     assert len(list(selection)) == len(tgraph.query_all_concepts())
 
+
 def test_query_new_concepts(selector):
     pass
+
+
+def test_query_concept_comp(selector, concept_name, fuzzy=True):
+    concept = selector.select('Concept').where(f"_.name =~ '.*{concept_name}.*")
