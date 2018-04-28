c964f8c5e406351a0139d0b47fe1a7f9efe05967
kellerx <keller0319@gmail.com>
2018-04-28 23:50:39 +0800

    minor change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='tdata',
     version='0.1',
-    author='xashes',
+    author='kellerx',
     author_email='kellerx0319@gmail.com',
     url='https://github.com/xashes/tdata',
     packages=find_packages(),
