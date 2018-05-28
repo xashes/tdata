e34cfc5f856e5014a9d9dfd1f484f5c70a06697e
xashes <kellerx0319@gmail.com>
2018-05-28 20:03:05 +0800

    convert daily and minute data to mongodb

--- tdata/db.py
+++ tdata/db.py
@@ -1,9 +1,11 @@
 import remote
 from pymongo import MongoClient
 import local
+from tqdm import tqdm
 
-CLIENT = MongoClient('mongodb://localhost:27017/')
+CLIENT = MongoClient()
 DB = CLIENT['tdata']
+
 
 def update_instruments_collection():
     stocks = remote.download_stock_table().to_dict(orient='records')
@@ -12,12 +14,14 @@ def update_instruments_collection():
     idx = remote.download_index_table().to_dict(orient='records')
     collection.insert_many(idx, ordered=False)
 
+
 def update_daily_collection():
     df = local.daily().reset_index().to_dict(orient='records')
     collection = DB['daily']
     collection.insert_many(df)
 
-def convert_minute_collection():
+
+def convert_minute_collection_old():
     df = local.bar('000001.SZ').reset_index()
     symbol = df.symbol[0]
     df = df.to_dict(orient='records')
@@ -25,10 +29,44 @@ def convert_minute_collection():
     collection.insert_many(df)
 
 
+def convert_minute_collection():
+    collection = DB['minute']
+    error_instruments = []
+    cursor = DB.instruments.find({}, {'symbol': 1, '_id': 0})
+    symbols = [c['symbol'] for c in cursor]
+    symbols = tqdm(symbols)
+    for symbol in symbols:
+        try:
+            df = local.bar(symbol)
+            df = df.reset_index().to_dict(orient='records')
+            collection.insert_many(df)
+        except (TypeError, IndexError):
+            print(symbol)
+            error_instruments.append(symbol)
+        with open('log.txt', 'a') as logger:
+            logger.writelines(error_instruments)
+
+def convert_daily_collection():
+    collection = DB['daily']
+    error_instruments = []
+    cursor = DB.instruments.find({}, {'symbol': 1, '_id': 0})
+    symbols = [c['symbol'] for c in cursor]
+    for symbol in symbols:
+        try:
+            df = local.daily(symbol)
+            df = df.reset_index().to_dict(orient='records')
+            collection.insert_many(df)
+        except (TypeError, IndexError):
+            print(symbol)
+            error_instruments.append(symbol)
+        with open('log.txt', 'a') as logger:
+            logger.writelines(error_instruments)
 
 def main():
-    # convert_minute_collection()
-    print(DB.minute['000001.SH'].find_one())
+    convert_minute_collection()
+    # convert_daily_collection()
+    # update_instruments_collection()
+
 
 if __name__ == '__main__':
     main()
