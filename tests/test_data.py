54aadab978fac749120324a49ac78c6f7b31aff4
xashes <xashes@outlook.com>
2018-10-03 09:16:08 +0800

    delete useless test files

--- tests/test_data.py
+++
@@ -1,7 +0,0 @@
-from tdata import local
-from tdata.remote_service import ds
-
-
-def test_daily_table():
-    assert len(local.daily('000001.SH', start_date=19901219)) == len(
-        ds.query_trade_dates(19901219, local.daily_last_date()))
