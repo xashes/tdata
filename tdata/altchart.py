f9465d95454766ea71878bb9ed91dc66ac660cae
xashes <xashes@outlook.com>
2018-10-05 10:46:14 +0800

    kline using altair

---
+++ tdata/altchart.py
@@ -0,0 +1,39 @@
+import altair as alt
+from tdata.features import Features
+
+
+def chart(df):
+    """
+    Input columns: 'open close low high turnover volume macd macdsignal
+    macdhist macdgrps macdhistgrps brushend bottom top'
+    """
+    df = df.reset_index()
+
+    # top
+    kline_color = alt.condition("datum.open < datum.close", alt.value('red'),
+                                alt.value('green'))
+    xaxis = alt.X('index:O', axis=alt.Axis(title='Date', grid=True, labelAngle=0))
+    rule = alt.Chart().mark_rule().encode(
+        xaxis,
+        alt.Y('low',
+              scale=alt.Scale(zero=False),
+              axis=alt.Axis(title='Price')
+        ),
+        alt.Y2('high'),
+        color=kline_color
+    )
+
+    bar = alt.Chart().mark_bar().encode(
+        xaxis,
+        y='open',
+        y2='close',
+        color=kline_color
+    )
+
+    kline = alt.layer(rule, bar).properties(data=df, width=1200, height=800)
+    kline.save('chart.html')
+    return kline
+
+if __name__ == '__main__':
+    df = Features().data()
+    chart(df)
--- tests/test_features.py
+++ tests/test_features.py
@@ -18,10 +18,30 @@ def test_bar(features):
 
 def test_data(features):
     df = features.data()
+    brushends = df.brushend.dropna()
+    bottoms = df.bottom.dropna()
+    tops = df.top.dropna()
 
     assert df.index[0] == datetime(2014, 2, 25)
     assert df.index[-1] == datetime(2018, 8, 1)
     assert df.shape == (1086, 14)
+
     assert 'brushend' in df.columns
     assert 'bottom' in df.columns
     assert 'top' in df.columns
+
+    assert brushends.shape == (34,)
+    assert brushends.min() == 1974.382
+    assert brushends.max() == 5178.191
+    assert brushends.loc['2014-02-25'] == 2087.6160
+    assert brushends.loc['2015-12-23'] == 3684.5674
+    assert brushends.loc['2018-07-06'] == 2691.0208
+
+    assert bottoms.shape == (4,)
+    assert bottoms.loc['2015-01-23'] == 2018.3600
+    assert bottoms.loc['2016-01-27'] == 3399.2803
+    assert bottoms.loc['2016-08-16'] == 2916.3744
+    assert bottoms.loc['2018-07-06'] == 3044.2912
+
+    assert tops.shape == (4,)
+    assert tops.loc['2018-07-06'] == 3140.4408
